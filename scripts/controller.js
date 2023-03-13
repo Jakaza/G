a37fc2f9fcd6971e3005d028cd0e13e576acb2be
Themba G <goodnessthembac@gmail.com>
2023-03-13 13:15:41 +0200

    Hide Visibility on small phone

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,33 +1,31 @@
-
-
 const lisElements = document.querySelectorAll('.service_side-btn')
 const inforElements = document.querySelectorAll('.infor')
 
 lisElements.forEach(button => {
 
-    button.addEventListener('click', (el)=>{
-        lisElements.forEach(btn => {btn.classList.remove('active');});
-        
+    button.addEventListener('click', (el) => {
+        lisElements.forEach(btn => { btn.classList.remove('active'); });
+
         const currentEl = el.target.classList
         currentEl.toggle("active")
-        if(currentEl.contains('web')){
+        if (currentEl.contains('web')) {
             displayInformation("web")
-        }else if(currentEl.contains('mobile')){
+        } else if (currentEl.contains('mobile')) {
             displayInformation("mobile")
-        }else if(currentEl.contains('mentor')){
+        } else if (currentEl.contains('mentor')) {
             displayInformation("mentor")
-        }else{
+        } else {
             displayInformation("none")
         }
     })
 })
 
 
-function displayInformation(title){
-    inforElements.forEach(infor =>{infor.classList.add("hide")})
+function displayInformation(title) {
+    inforElements.forEach(infor => { infor.classList.add("hide") })
 
-    inforElements.forEach(infor=>{
-        if(infor.classList.contains(title)){
+    inforElements.forEach(infor => {
+        if (infor.classList.contains(title)) {
             infor.classList.remove('hide');
         }
     })
