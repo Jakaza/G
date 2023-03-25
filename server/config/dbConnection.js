b62d3b0285f8c54f6671dab97024b3ed4e74599a
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-03-25 21:24:12 +0200

    Update dbConnection.js

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+/*const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
@@ -11,6 +11,7 @@ const connectDB = async () => {
     }
 }
 module.exports = connectDB;
+*/
 
 
 
