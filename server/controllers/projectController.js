7224a155a33171bde7b1b7b778b5e44dbf4fa91c
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-03-28 12:42:16 +0200

    controllers to update project show and hilde handlers

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,9 +1,40 @@
 const newProject = (req, res) => {
     //Add new project to database
 
+}
 
 
+const hideProject = async (req, res)=>{
+    const filter = { id: req.body.id };
+    const update = false;
+    try {
+        const result = await Project.findOneAndUpdate(filter, update, {
+            new: true,
+            upsert: true,
+            rawResult: true // Return the raw result from the MongoDB driver
+          })
+    } catch (error) {
+        console.log('Failed to update');
+    }
+}
+
+const showProject = async (req, res)=>{
+    const filter = { id: req.body.id };
+    const update = true;
+    try {
+        const result = await Project.findOneAndUpdate(filter, update, {
+            new: true,
+            upsert: true,
+            rawResult: true // Return the raw result from the MongoDB driver
+          })
+    } catch (error) {
+        console.log('Failed to update');
+    }
+}
 
 
-
+module.exports = {
+    hideProject,
+    showProject,
+    newProject
 }
--- server/routes/api/project.js
+++ server/routes/api/project.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const mailController = require('../../controllers/mailController')
+const projectController = require('../../controllers/projectController')
 const validateInput = require('../../middleware/validateInput')
 
 router.post('/api/project/new')
+
+router.post('/api/project/hide/:id', projectController.hideProject)
+
+router.post('/api/project/show/:id' , projectController.showProject )
 // router.delete()
 // router.put()
 
 
+
+// mini project
+
 router.post('/api/miniproject/new')
 
+router.post('/api/miniproject/hide/:id')
+
+router.post('/api/miniproject/show/:id')
 
 module.exports = router;
