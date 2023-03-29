341a26ade0648924fe371d929056833cb4d88e06
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2023-03-29 21:21:13 +0200

    Update dbConnection.js
    remove useCreateIndex option

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URI, 
-            {   dbName: 'portfolio',         
-                useCreateIndex: true, //make this true
+            {   dbName: 'portfolio', 
                 autoIndex: true, //make this also true
              })
         console.log('DB Connected.')
