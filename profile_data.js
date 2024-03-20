2b41343003eaaf47e216a9fb57967ed4376e60b7
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-03-20 13:49:34 +0200

    Create profile_data.js

---
+++ profile_data.js
@@ -0,0 +1,14 @@
+const profileData = {
+    username: "THEMBA",
+    role: "Software Engineer Intern",
+    bio: "As a dedicated software engineer intern, I am passionate about creating high quality software solutions that meet the needs of the users. I have a deep understanding of programming languages, framework and software development methodologies, and I am always eager to learn new technologies and tools. I enjoy collaborating with other engineers and working in a team.",
+    email: "goodnessthembac@gmail.com",
+    linkedin: "https://linkedin.com/in/g-themba-chauke",
+    twitter: "https://twitter.com/ThembaG3",
+    github: "https://github.com/Jakaza",
+    contact: "071-1770-423"
+  };
+
+
+module.exports = profileData
+  
