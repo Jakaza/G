c3fa7b4386ecc0ba73817cf9eba891abc528fb0c
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-03-20 13:51:06 +0200

    Update dbConnection.js
    add profile

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const Profile = require('../model/profile')
+const profileData = require('../../profile_data')
 
 const connectDB = async () => {
   try {
@@ -8,6 +10,14 @@ const connectDB = async () => {
         autoIndex: true, //make this also true
       })
     console.log('DB Connected.')
+
+    const existingProfile = await Profile.findOne({ username: profileData.username });
+    if (!existingProfile) {
+      const newProfile = new Profile(profileData);
+      await newProfile.save();
+      console.log(`Inserted default profile document`);
+    }
+    console.log(`Default profile EXISTS`);
 
   } catch (error) {
     console.error(error)
